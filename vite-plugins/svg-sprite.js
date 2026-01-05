bc450f03cdd4a3a3440443a14bdb12c221b044ae
ivan-mr1 <ivanitit3535@gmail.com>
2026-01-05 21:45:27 +0200

    feat: add monochrome sprite

--- vite-plugins/svg-sprite.js
+++ vite-plugins/svg-sprite.js
@@ -1,35 +1,32 @@
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 import path from 'path';
-import process from 'node:process'; // Добавляем импорт для исправления ошибки
+import VitePluginSvgSpritemap from '@spiriit/vite-plugin-svg-spritemap';
 
-export const svgSpritePlugin = () =>
-  createSvgIconsPlugin({
-    // Путь к папке с иконками
-    iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+export const svgSpritePlugin = () => {
+  const iconsPath = path
+    .resolve(process.cwd(), 'src/assets/icons/monochrome/*.svg')
+    .replace(/\\/g, '/');
 
-    // Формат ID: icon-monochrome-name или icon-colored-name
-    symbolId: 'icon-[dir]-[name]',
+  console.log('\n--- SVG SPRITE DEBUG ---');
+  console.log('Searching for icons in:', iconsPath);
+  console.log('------------------------\n');
 
-    svgoOptions: {
-      full: true,
-      plugins: [
-        {
-          name: 'removeAttrs',
-          params: { attrs: '(fill|stroke)' },
-          // Логика: если файл в папке 'colored', отменяем удаление атрибутов
-          fn: (item, params, info) => {
-            if (info.path && info.path.includes('colored')) {
-              return null;
-            }
+  return [
+    VitePluginSvgSpritemap(iconsPath, {
+      output: {
+        filename: 'spritemap.svg',
+      },
+      prefix: 'sprite-',
+      injectSvgOnDev: true,
+      svgo: {
+        plugins: [
+          {
+            name: 'removeAttrs',
+            params: {
+              attrs: '(fill|stroke)',
+            },
           },
-        },
-        'removeXMLNS',
-        'removeDimensions',
-        // Оставляем viewBox, чтобы иконки масштабировались корректно
-        {
-          name: 'removeViewBox',
-          active: false,
-        },
-      ],
-    },
-  });
+        ],
+      },
+    }),
+  ];
+};
